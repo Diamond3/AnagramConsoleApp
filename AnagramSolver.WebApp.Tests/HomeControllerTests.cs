e03fa283bfc54452cbd1f11fae717b3718d9e169
Deimantas Eidukevičius <deimantas.eidukevici@visma.com>
2022-05-18 12:48:39 +0300

    Add HomeController tests

---
+++ AnagramSolver.WebApp.Tests/HomeControllerTests.cs
@@ -0,0 +1,92 @@
+using System.Collections.Generic;
+using AnagramSolver.Contracts.Interfaces;
+using AnagramSolver.Contracts.Models;
+using AnagramSolver.WebApp.Controllers;
+using AnagramSolver.WebApp.Models;
+using System.Web;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using Shouldly;
+
+namespace AnagramSolver.WebApp.Tests;
+
+public class HomeControllerTests
+{
+    private HomeController _homeController;
+    [SetUp]
+    public void SetUp()
+    {
+        var wordServiceMock = new Mock<IWordService<Word>>();
+        var cookieServiceMock = new Mock<ICookieService>();
+        var userServiceMock = new Mock<IUserService>();
+        
+        var wordsList = new List<Word>()
+        {
+            new Word() { FirstForm = "zzodis" },
+            new Word() { FirstForm = "kitasZodis" }
+        };
+
+        _homeController = new HomeController(wordServiceMock.Object, cookieServiceMock.Object, userServiceMock.Object);
+        
+        wordServiceMock.Setup(x => x.GetAllWords())
+            .Returns(wordsList);
+        wordServiceMock.Setup(x => x.GetAnagrams(It.IsAny<string>()))
+            .Returns(wordsList);
+        
+        cookieServiceMock.Setup(x => x.GetCount(It.IsAny<string>()))
+            .Returns(0);
+        cookieServiceMock.Setup(x => x.CanAddValue(It.IsAny<string>(), It.IsAny<List<string>>()))
+            .Returns(false);
+        
+        userServiceMock.Setup(x => x.AbleToDoAction(It.IsAny<string>()))
+            .Returns(true);
+    }
+
+    
+    [Explicit, Test]
+    public void Index_ShouldReturnAnagramsList()
+    {
+        //Arrange
+        var expectedAnagramsList = new List<Word>()
+        {
+            new Word() { FirstForm = "kitasZodis" },
+            new Word() { FirstForm = "zzodis" }
+        };
+        var expectedWord = "zzodis";
+
+        //Act
+        var viewResult = _homeController.Index(expectedWord) as ViewResult;
+        var wordList = (WordList)viewResult.Model;
+
+        var actualAnagramsList = wordList.Anagrams;
+        var actualWord = wordList.Word;
+        
+        //Assert
+        actualAnagramsList.ShouldBe(expectedAnagramsList, ignoreOrder: true);
+        actualWord.ShouldBe(expectedWord);
+    }
+    
+    [Test]
+    public void SearchAnagramsWithInfo_ReturnsModelWithAllAnagrams()
+    {
+        //Arrange
+        var expectedAnagramsList = new List<Word>()
+        {
+            new Word() { FirstForm = "kitasZodis" },
+            new Word() { FirstForm = "zzodis" }
+        };
+        var expectedWord = "zzodis";
+
+        //Act
+        var viewResult = _homeController.SearchWordsInfo(expectedWord) as ViewResult;
+        var userInfo = (UserInfo)viewResult.Model;
+
+        var actualAnagramsList = userInfo.WordList.Anagrams;
+        var actualWord = userInfo.WordList.Word;
+        
+        //Assert
+        actualAnagramsList.ShouldBe(expectedAnagramsList, ignoreOrder: true);
+        actualWord.ShouldBe(expectedWord);
+    }
+}
